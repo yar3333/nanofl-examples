571b68b16346b2f4c99f941142e708ffc2c4c1d7
Yaroslav Sivakov <yar3333@gmail.com>
2016-08-21 23:12:14 +0300

    * upgrade most examples

---
+++ likebutton/src/MyButton.hx
@@ -0,0 +1,17 @@
+class MyButton extends base.MyButton
+{
+	function init()
+	{
+		// your initialization code
+	}
+	
+	override function onEnterFrame()
+	{
+		// your code for tick
+	}
+	
+	override function onMouseDown(e:createjs.MouseEvent)
+	{
+		// your code for mouse down
+	}
+}
